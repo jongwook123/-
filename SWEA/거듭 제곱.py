9c671fa3b4ecc6d8fea09dcf5566562f73d468a4
Jongwook <pju7429@naver.com>
2023-03-30 11:27:39 +0900

    파일수정

--- SWEA/거듭 제곱.py
+++
@@ -1,8 +0,0 @@
-for tc in range(1,11):
-    _ = input()
-    N,M = map(int,input().split())
-    a = N
-    for i in range(M-1):
-        a = a * N
-
-    print(f'#{tc} {a}')
--- SWEA/숫자사각형4.py
+++
@@ -1,8 +0,0 @@
-T = int(input())
-for tc in range(1, T + 1):
-    N = int(input())
-    print(f'#{tc}')
-    for s in range(1,N+1):
-        for n in range(1,N+1):
-            print(f'{n*s} ',end='')
-        print()
