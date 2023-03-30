a1b80b81f4c93f3764f1e6bcdd91f517d251618f
jongwook123 <122584199+jongwook123@users.noreply.github.com>
2023-03-30 10:51:51 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,19 @@
+
+T = int(input())
+for tc in range(T):
+    Stack = []
+    A = list(input())
+    for a in A:
+        if a == '(':
+            Stack.append(a)
+        else:
+            if len(Stack) == 0:
+                print('NO')
+                break
+            else:
+                Stack.pop()
+    else:
+        if len(Stack) == 0:
+            print('YES')
+        else:
+            print('NO')
