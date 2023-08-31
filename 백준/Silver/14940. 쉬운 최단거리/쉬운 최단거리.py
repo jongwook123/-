c6d46045fd1f529cfad9b3d60df13ef10a9d998a
jongwook123 <122584199+jongwook123@users.noreply.github.com>
2023-08-31 11:04:37 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 592 ms, Memory: 41656 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,11 +1,17 @@
 from collections import deque
 
-def bfs():
+def find_start():
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 2:
+                return i,j
+
+def bfs(i,j):
     Q = deque()
-    Q.append((start_i, start_j))
-    visited[start_i][start_j] = True
+    Q.append((i,j))
+    visited[i][j] = 1
     while Q:
-        i, j = Q.popleft()
+        i,j = Q.popleft()
 
         for s in range(4):
             ni = i + di[s]
@@ -13,26 +19,21 @@ def bfs():
             if 0 <= ni < N and 0 <= nj < M:
                 if arr[ni][nj] == 1 and not visited[ni][nj]:
                     arr_1[ni][nj] = arr_1[i][j] + 1
-                    Q.append((ni, nj))
-                    visited[ni][nj] = True
+                    Q.append((ni,nj))
+                    visited[ni][nj] = 1
 
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-arr_1 = [[0] * M for _ in range(N)]
-visited = [[False] * M for _ in range(N)]
-di = [1, 0, 0, -1]
-dj = [0, 1, -1, 0]
 
-start_i, start_j = None, None
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 2:
-            start_i, start_j = i, j
-            break
-    if start_i is not None:
-        break
 
-bfs()
+
+N,M = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(N)]
+arr_1 = [[0]*M for _ in range(N)]
+visited = [[0]*M for _ in range(N)]
+di = [1,0,0,-1]
+dj = [0,1,-1,0]
+start_i,start_j = find_start()
+
+bfs(start_i,start_j)
 
 for i in range(N):
     for j in range(M):
