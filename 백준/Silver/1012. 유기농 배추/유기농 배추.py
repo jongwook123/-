393ecf39f6e4a3ca765a74af1c066c3708ca56c2
jongwook123 <122584199+jongwook123@users.noreply.github.com>
2024-01-08 15:33:55 +0900

    [Silver II] Title: 유기농 배추, Time: 316 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,36 +1,45 @@
 from collections import deque
-def bfs(i,j):
+
+# 4방향 탐색
+di = [0,0,1,-1]
+dj = [-1,1,0,0]
+
+def bfs(i,j,arr,Visited):
     Q = deque()
     Q.append((i,j))
-    visited[0][0] = 1
+    Visited[i][j] = 1 # 방문체크
+
     while Q:
         i,j = Q.popleft()
+
         for s in range(4):
             ni = i + di[s]
             nj = j + dj[s]
-            if 0 <= ni < N and 0 <=nj < M:
-                if arr[ni][nj] == 1 and not visited[ni][nj]:
-                    visited[ni][nj] = 1
+            if 0 <= ni < N and 0 <= nj < M and arr[ni][nj] == 1:
+                if Visited[ni][nj] == 0:
                     Q.append((ni,nj))
+                    Visited[ni][nj] = 1
 
-di = [1,-1,0,0]
-dj = [0,0,1,-1]
 
-T = int(input())
-for tc in range(1,T+1):
-    M,N,K = map(int,input().split())    # M : 가로 N : 세로
-    A = max(M,N)
-    cnt = 0
-    arr = [[0] * (M+1) for _ in range(N+1)]
-    visited = [[0] * (M+1) for _ in range(N+1)]
-    for _ in range(K):
-        s,e = map(int,input().split())
-        arr[e][s] = 1
 
-    for i in range(N):
-        for j in range(M):
-            if arr[i][j] == 1 and not visited[i][j]:
-                bfs(i,j)
-                cnt += 1
 
-    print(cnt)
+T = int(input()) # 총 테스트 케이스
+for _ in range(T):
+    M, N, K = map(int,input().split()) # 가로, 세로, 지렁이 갯수
+    arr = [[0] * M for _ in range(N)] # 배추가 심어진 전체 땅
+    Visited = [[0] * M for _ in range(N)] # 방문표시
+    Ji = 0 # 지렁이 갯수
+
+    for _ in range(K): # 지렁이가 있는 땅 
+        i,j = map(int,input().split())
+        arr[j][i] = 1 # 지렁이 기록 
+    
+    # -> K 입력이 세로 가로 바껴있어서 인덱스 오류가 났음 문제 잘 읽어볼것.
+
+    for j in range(M):
+        for i in range(N):
+            if arr[i][j] and Visited[i][j] == 0: # 지렁이가 있고 방문을 하지 않았다면
+                bfs(i,j,arr,Visited)
+                Ji += 1
+
+    print(Ji)
