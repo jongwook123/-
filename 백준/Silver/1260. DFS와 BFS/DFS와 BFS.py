12c5adaca1651a94d4d938d3d5c1233d55fb4c16
jongwook123 <122584199+jongwook123@users.noreply.github.com>
2024-01-09 17:38:08 +0900

    [Silver II] Title: DFS와 BFS, Time: 460 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,32 +1,42 @@
-def bfs(v):
-    Q = []
-    Q.append(v)
-    visited[v] = 1
+from collections import deque
+
+
+def dfs(d,visited,result):
+    result.append(d)
+    visited[d] = 1
+
+    for s in arr[d]:
+        if not visited[s]:
+            dfs(s,visited,result)
+
+    return result
+
+def bfs(b):
+    Q = deque()
+    Q.append(b)
+    Visited[b] = 1
+    B = []
     while Q:
-        v = Q.pop(0)
-        print(v,end = ' ')
-        for w in range(1,N+1):
-            if visited[w] == 0 and arr[v][w] == 1:
-                Q.append(w)
-                visited[w] = 1
+        t = Q.popleft()
+        B.append(t)
+        for s in arr[t]:
+            if Visited[s] == 0:
+                Q.append(s)
+                Visited[s] = 1
 
-
-def dfs(v):
-    visited[v] = 1
-    print(v,end=' ')
-    for w in range(1,N+1):
-        if visited[w] == 0 and arr[v][w]==1:
-            dfs(w)
-
-
+    return B
 N,M,V = map(int,input().split())
-arr = [[0] * (N+1) for _ in range(N+1)]
-visited =[0] *(N+1)
-for _ in range(M):
-    A,B = map(int,input().split())
-    arr[A][B] = 1
-    arr[B][A] = 1
-dfs(V)
-print()
-visited =[0] *(N+1)
-bfs(V)
+arr = {i : [] for i in range(N + 1)}
+Visited = [0] * (N+1)
+for s in range(M):
+    a, b = map(int,input().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+for s in range(N+1):
+    arr[s].sort()
+
+print(*dfs(V,Visited,[]))
+Visited = [0] * (N+1)
+print(*bfs(V))
+
